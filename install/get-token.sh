b1d26cbe2cd1edc2438dfce6501c71a9fe12f846
Sila Kissuu <sila@kissuu.com>
2024-10-09 14:56:18 -0400

    fix token logic

--- install/get-token.sh
+++ install/get-token.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 # Set your OpenShift cluster's API URL and your credentials
 API_URL="https://api.ocp.ibm.edu:6443"
@@ -8,12 +8,12 @@ PASSWORD="ibmrhocp"
 
 # Log in to OpenShift and get the login token
 #TOKEN=$(oc login $API_URL --username=$USERNAME --password=$PASSWORD --token-only 2>/dev/null)
-TOKEN=$(oc login $API_URL --username=$USERNAME --password=$PASSWORD --insecure-skip-tls-verify)
+#oc login $API_URL --username=$USERNAME --password=$PASSWORD --insecure-skip-tls-verify
+oc login $API_URL --username=$USERNAME --password=$PASSWORD 
 
 # Check if the login was successful
 if [ $? -eq 0 ]; then
-    echo "Login successful. Your token is:"
-    echo "$TOKEN"
+    echo "Login successful...retrieving login token"
 else
     echo "Login failed. Please check your credentials or API URL."
     exit 1
@@ -24,12 +24,12 @@ TOKEN=$(oc whoami -t)
 
 # Check if token retrieval was successful
 if [ $? -ne 0 ]; then
-    echo "Failed to retrieve the token."
+    echo "Failed to retrieve login token."
     exit 1
+else
+    echo "Login token: $TOKEN"
 fi
 
 # Output the token
-echo "Login token: $TOKEN"
-
 # Optionally, you can save the token to a file
 # echo "$TOKEN" > openshift_token.txt
