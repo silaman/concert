f318fc20fc5777a3bba438c0fc338f04bf971553
Sila Kissuu <sila@kissuu.com>
2024-11-01 12:57:37 -0500

    Update deploy-concert.sh

--- install/deploy-concert.sh
+++ install/deploy-concert.sh
@@ -4,9 +4,9 @@ export WORK_DIR=/home/ibmuser/concert
 export OCP_URL=https://api.ocp.ibm.edu:6443
 export PROJECT_OPERATOR=aaf
 export PROJECT_INSTANCE=concert
-export STG_CLASS_BLOCK=ocs-storagecluster-ceph-rbd
-export STG_CLASS_FILE=ocs-storagecluster-ceph-rgw
-export IBM_ENTITLEMENT_KEY=eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJJQk0gTWFya2V0cGxhY2UiLCJpYXQiOjE2MDE5MzU5MzIsImp0aSI6IjJjN2U3ZWJmNDkwYjRmODVhYWY1OGFhNmQ3NWI4ODYxIn0.SB8ceB6b630fG_mU-vGzkffAdLi9YhMEZK4maH10nF0
+export STG_CLASS_BLOCK=managed-nfs-storage
+export STG_CLASS_FILE=managed-nfs-storage
+export IBM_ENTITLEMENT_KEY=
 export OCP_PASSWORD=ibmrhocp
 export OCP_USERNAME=ocadmin
 
